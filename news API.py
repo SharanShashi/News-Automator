83d4e981280f05d3751dfb9377044cc2a9a2e009
SharanShashi <163171365+SharanShashi@users.noreply.github.com>
2024-03-28 00:49:36 +0530

    Add files via upload
    API1

---
+++ news API.py
@@ -0,0 +1,84 @@
+import requests
+import io
+import webbrowser
+from tkinter import *
+from urllib.request import urlopen
+from PIL import ImageTk, Image 
+
+class NewsApp:
+
+    def __init__(self):
+         
+        # fetch data
+        self.data = requests.get('https://newsapi.org/v2/top-headlines?country=in&apiKey=6c6c39d4a147401ea4446d239d0cfc40').json()
+        # intital GUI load
+        self.load_gui()
+        # load the 1st news item
+        self.load_news_item(0) 
+
+
+    def load_gui(self):
+        self.root = Tk()
+        self.root.geometry('350x600')
+        self.root.resizable(0,0)
+        self.root.title('My NEWS API APP')
+        self.root.configure(background='black')
+    
+    def clear(self):
+        for i in self.root.pack_slaves():
+            i.destroy()
+
+    def load_news_item(self,index):
+
+        #clear the screen for the new news item
+        self.clear()
+
+        #image
+        try:
+            img_url = self.data['articles'][index]['urlToImage']
+            raw_data = urlopen(img_url).read()
+            im = Image.open(io.BytesIO(raw_data)).resize((350,250))
+            photo = ImageTk.PhotoImage(im)
+        except:
+            img_url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfId1JtxLoc7bKkkSfmxtlc9ti_2Hhhcpm_7cd5yWQZANmFi0pELuxzHUgf_bzo4NSAuM&usqp=CAU'
+            raw_data = urlopen(img_url).read()
+            im = Image.open(io.BytesIO(raw_data)).resize((350,250))
+            photo = ImageTk.PhotoImage(im)
+    
+        label = Label(self.root,image=photo)
+        label.pack()
+
+
+        heading = Label(self.root,text=self.data['articles'][index]['title'],bg='black',fg='white',wraplength=350,justify='center')
+        heading.pack(pady=(10,20))
+        heading.config(font=('verdana',15))
+
+        details = Label(self.root,text=self.data['articles'][index]['description'],bg='black',fg='white',wraplength=350,justify='center')
+        details.pack(pady=(2,20))
+        details.config(font=('verdana',12))
+
+        frame = Frame(self.root,bg ='black')
+        frame.pack(expand=True,fill=BOTH)
+
+
+        if index !=0:
+            prev = Button(frame,text='prev',width=16,height=3,command=lambda :self.load_news_item(index-1))
+            prev.pack(side=LEFT)
+ 
+        read = Button(frame,text='Read More',width=16,height=3,command=lambda :self.open_link(self.data['articles'][index]['url']))
+        read.pack(side=LEFT)
+
+
+        if index != len(self.data['articles'])-1:
+            next = Button(frame,text='Next',width=16,height=3,command=lambda :self.load_news_item(index+1))
+            next.pack(side=LEFT)
+
+    
+
+        self.root.mainloop()
+
+    def open_link(self,url):
+        webbrowser.open(url)
+
+obj = NewsApp()
+     
